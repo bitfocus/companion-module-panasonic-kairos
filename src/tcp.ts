1e18befab859bc826f04d584a8853b73347b4640
JeffreyDavidsz <jeffrey.davidsz@vicreo.eu>
2022-03-12 23:45:25 +0100

    remove extra socket, data in serial requests

--- src/tcp.ts
+++ src/tcp.ts
@@ -1,28 +1,17 @@
 import tcp from '../../../tcp'
 import KairosInstance from '.'
 
-// interface MessageBuffer {
-//   dataLength: number
-//   message: Buffer
-// }
-
 // OK, Warning, Error, Unknown
 type TCPStatus = 0 | 1 | 2 | null
 
 interface TCPSockets {
   main: tcp | null
-  subscribe: tcp | null
 }
 
 export class TCP {
   private readonly instance: KairosInstance
-  // private messageBuffer: MessageBuffer = {
-  //   dataLength: 0,
-  //   message: Buffer.from(''),
-  // }
   private sockets: TCPSockets = {
     main: null,
-    subscribe: null,
   }
   private tcpHost: string
   private tcpPort: number
@@ -33,29 +22,22 @@ export class TCP {
     this.tcpHost = instance.config.host
     this.tcpPort = instance.config.port
 
-    let inputs = []
-    for (let index = 1; index < 49; index++) {
-      inputs.push({ input: `IP${index}`, name: '' })
-      if (index < 33) inputs.push({ input: `SDI${index}`, name: '' })
-      if (index < 7) inputs.push({ input: `STREAM${index}`, name: '' })
-    }
-    inputs.push({ input: `NDI1`, name: '' })
-    inputs.push({ input: `NDI2`, name: '' })
-		inputs.sort(function(a, b) {
-			let keyA = a.input,	keyB = b.input
-			if (keyA < keyB) return -1;
-			if (keyA > keyB) return 1;
-			return 0;
-		})
-		
+    // Want to Keep this as reminder for sorting key pair array's
+    // inputs.sort(function(a, b) {
+    // 	let keyA = a.input,	keyB = b.input
+    // 	if (keyA < keyB) return -1;
+    // 	if (keyA > keyB) return 1;
+    // 	return 0;
+    // })
+
     this.instance.KairosObj = {
       main_background_sourceA: '',
       main_background_sourceB: '',
       audio_master_mute: 0,
-      INPUTS: inputs,
+      INPUTS: [{ input: '', name: '', live: false }],
       SCENES: [],
       SNAPSHOTS: [],
-      AUX: [],
+      AUX: [{ aux: '', live: '', sources: [] }],
       MACROS: [],
       PLAYERS: [
         { player: 'RR1', repeat: 0 },
@@ -97,7 +79,6 @@ export class TCP {
    */
   public readonly destroy = (): void => {
     if (this.sockets.main) this.sockets.main.destroy()
-    if (this.sockets.subscribe) this.sockets.subscribe.destroy()
     if (this.keepAliveInterval != undefined) clearInterval(this.keepAliveInterval)
   }
 
@@ -115,7 +96,6 @@ export class TCP {
 
     // The functions socket is primary and controls the module status and startup of activator and xml sockets
     this.sockets.main = new tcp(this.tcpHost, this.tcpPort)
-    this.sockets.subscribe = new tcp(this.tcpHost, this.tcpPort)
 
     this.sockets.main.on('status_change', (status: TCPStatus, message: string) => {
       let state: 0 | 1 | 2 | null = this.instance.STATUS_UNKNOWN
@@ -130,55 +110,38 @@ export class TCP {
     this.sockets.main.on('error', (err: Error) => {
       this.instance.status(this.instance.STATUS_ERROR, err.message)
     })
-
-    this.sockets.subscribe.on('error', (err: Error) => {
-      this.instance.status(this.instance.STATUS_ERROR, err.message)
-    })
-
-    function delay(time: number) {
+    // Helpers
+    const delay = (time: number) => {
       return new Promise((resolve) => setTimeout(resolve, time))
     }
 
-    this.sockets.subscribe.on('connect', () => {
-      this.instance.status(0)
-      this.instance.log('debug', 'via subscription')
-      this.sendCommandSubscribeChannel('Subscribe:Mixer.AudioMixers.AudioMixer.mute')
-      delay(200)
-        .then(() => {
-          this.instance.KairosObj.PLAYERS.forEach((element) => {
-            this.sendCommandSubscribeChannel(`subscribe:${element.player}.repeat`)
-          })
-        })
-        .then(() =>
-          delay(200).then(() => {
-            this.instance.KairosObj.AUDIO_CHANNELS.forEach((element) => {
-              this.sendCommandSubscribeChannel(`subscribe:Mixer.AudioMixers.AudioMixer.${element.channel}.mute`)
-            })
-          })
-        )
-        .then(() =>
-          delay(200).then(() => this.sendCommandSubscribeChannel('subscribe:SCENES.Main.Layers.Background.sourceA'))
-        )
-        .then(() =>
-          delay(200).then(() => this.sendCommandSubscribeChannel('subscribe:SCENES.Main.Layers.Background.sourceB'))
-        )
-    })
+    const sendCommandWithDelay = async (command: string, milisec: number) => {
+      // We can await a function that returns a promise
+      this.sendCommand(command)
+      await delay(milisec)
+    }
 
     let fetchInitialData = () => {
       return new Promise((resolve) => {
-        let waitForArray = new Promise((res) => {
-          this.instance.KairosObj.INPUTS.forEach((element, index, array) => {
-            delay(200).then(() => this.sendCommand(`${element.input}.name`))
-            if (index === array.length - 1) res('ready')
-          })
-        })
-        waitForArray
-          .then(() => delay(300).then(() => this.sendCommand('list:AUX')))
+        // 300 milisec is save, if you put it lower it will break
+        delay(10)
+          .then(() => this.sendCommand('list:AUX'))
+          .then(() => delay(300).then(() => this.sendCommand('list:Mixer.Inputs')))
           .then(() => delay(300).then(() => this.sendCommand('list:SCENES')))
           .then(() => delay(300).then(() => this.sendCommand('list:MACROS')))
           .then(() => delay(300).then(() => this.sendCommand('list:SCENES.Main.Snapshots')))
           .then(() => delay(300).then(() => this.sendCommand('list:Mixer.MV-Presets')))
-          .then(() => resolve('Resolving'))
+          .then(async () => {
+            // Fetch all source options for aux
+            for (const item of this.instance.KairosObj.AUX) {
+              if (item.aux !== '') await sendCommandWithDelay(`${item.aux}.sourceOptions`, 300)
+            }
+            // Fetch all input names
+            for (const iterator of this.instance.KairosObj.INPUTS) {
+              if (iterator.input !== '') await sendCommandWithDelay(`${iterator.input}.name`, 300)
+            }
+            delay(300).then(() => resolve)
+          })
       })
     }
 
@@ -186,6 +149,23 @@ export class TCP {
       this.instance.status(0)
       this.instance.log('debug', 'Connected to mixer')
       fetchInitialData().then(() => {
+        // All data is in, now Subscribe to some stuff
+        this.sendCommand('Subscribe:Mixer.AudioMixers.AudioMixer.mute')
+        delay(200)
+          .then(() => {
+            this.instance.KairosObj.PLAYERS.forEach((element) => {
+              this.sendCommand(`subscribe:${element.player}.repeat`)
+            })
+          })
+          .then(() =>
+            delay(200).then(() => {
+              this.instance.KairosObj.AUDIO_CHANNELS.forEach((element) => {
+                this.sendCommand(`subscribe:Mixer.AudioMixers.AudioMixer.${element.channel}.mute`)
+              })
+            })
+          )
+          .then(() => delay(200).then(() => this.sendCommand('subscribe:SCENES.Main.Layers.Background.sourceA')))
+          .then(() => delay(200).then(() => this.sendCommand('subscribe:SCENES.Main.Layers.Background.sourceB')))
         this.instance.updateInstance()
         this.keepAliveInterval = setInterval(keepAlive, 4500) //session expires at 5 seconds
       })
@@ -193,12 +173,11 @@ export class TCP {
 
     let keepAlive = () => {
       this.sendCommand('')
-      this.sendCommandSubscribeChannel('')
     }
 
     this.sockets.main.on('data', (data: Buffer) => {
       //create array from data
-      const message = data.toString().trim().split('\r\n')
+      const message = data.toString().split('\r\n')
 
       if (message.find((element) => element == 'OK')) {
         //Status message
@@ -206,54 +185,7 @@ export class TCP {
       } else if (message.find((element) => element == 'Error')) {
         //Status message
         this.instance.log('debug', 'Command failed')
-      } else if (message.find((element) => element == 'IP1')) {
-        //This is an input list
-        console.log(message)
-
-        // message.forEach((element) => {
-        //   this.instance.KairosObj.INPUTS.push({ input: element, name: '' })
-        // })
-      } else if (message.find((element) => element.includes('.name='))) {
-        //This is an name for an Input (BE AWARE THIS CAN CHANGE IN THE FUTURE)
-        message.forEach((res) => {
-          let input = res.split('=')[0].slice(0, -5)
-          let name = res.split('=')[1]
-          for (const key in this.instance.KairosObj.INPUTS) {
-            if (Object.prototype.hasOwnProperty.call(this.instance.KairosObj.INPUTS, key)) {
-              const inputFromArray = this.instance.KairosObj.INPUTS[key]
-              if (inputFromArray.input == input) {
-                inputFromArray.name = name
-                break
-              }
-            }
-          }
-        })
-      } else if (message.find((element) => element == 'IP-AUX1')) {
-        //This is an AUX list
-        this.instance.KairosObj.AUX = message
-      } else if (message.find((element) => element.includes('MACROS.'))) {
-        //This is an MACRO list
-        this.instance.KairosObj.MACROS = message
-      } else if (message.find((element) => element.includes('Mixer.MV-Presets.'))) {
-        //This is an MV Preset list
-        this.instance.KairosObj.MV_PRESETS = message
-      } else if (message.find((element) => element.includes('SCENES.Main.Snapshots'))) {
-        //This is an SNAPSHOT list
-        this.instance.KairosObj.SNAPSHOTS = message
-      } else if (message.find((element) => element == 'SCENES.Main')) {
-        //This is an SCENES list
-        this.instance.KairosObj.SCENES = message
-      } else {
-        console.log(message)
-      }
-
-      // this.instance.log('debug', `Command Response: ${message}`)
-    })
-    this.sockets.subscribe.on('data', (data: Buffer) => {
-      //create array from data
-      const message = data.toString().trim().split('\r\n')
-
-      if (message[0].split('=')[0] == 'SCENES.Main.Layers.Background.sourceA') {
+      } else if (message[0].split('=')[0] == 'SCENES.Main.Layers.Background.sourceA') {
         this.instance.KairosObj.main_background_sourceA = message[0].split('=')[1]
         this.instance.variables?.updateVariables()
         this.instance.checkFeedbacks('inputSourceA')
@@ -272,11 +204,65 @@ export class TCP {
         let channelIndex = parseInt(message[0].slice(29, -5)) - 1
         this.instance.KairosObj.AUDIO_CHANNELS[channelIndex].mute = parseInt(message[0].split('=')[1])
         this.instance.checkFeedbacks('audioMuteChannel')
+      } else if (message.find((element) => element == 'IP1')) {
+        //This is an input list
+        console.log(message)
+        if (message.length > 0) {
+          this.instance.KairosObj.INPUTS.length = 0
+          message.forEach((element) => {
+            if (element !== '') this.instance.KairosObj.INPUTS.push({ input: element, name: element, live: false })
+          })
+        }
+      } else if (message.find((element) => element.includes('.name='))) {
+        //This is an name for an Input (BE AWARE THIS CAN CHANGE IN THE FUTURE)
+        message.forEach((res) => {
+          if (res !== '') {
+            let input = res.split('=')[0].slice(0, -5)
+            let name = res.split('=')[1]
+            for (const key in this.instance.KairosObj.INPUTS) {
+              if (Object.prototype.hasOwnProperty.call(this.instance.KairosObj.INPUTS, key)) {
+                const inputFromArray = this.instance.KairosObj.INPUTS[key]
+                if (inputFromArray.input == input) {
+                  inputFromArray.name = name
+                  break
+                }
+              }
+            }
+          }
+        })
+      } else if (message[0].search('.sourceOptions') != -1 && message[0].search('AUX') != -1) {
+        //This is Aux source option
+        // Split data for name and options
+        let auxSourceOptions = message[0].split('=')
+        // aux name/ID is the first part
+        let auxName = auxSourceOptions[0].slice(0, -14)
+
+        // When ready remove the first part so you keep only the options
+        auxSourceOptions.shift()
+
+        let index = this.instance.KairosObj.AUX.findIndex((x) => x.aux === auxName)
+        if (index != -1) this.instance.KairosObj.AUX[index].sources = auxSourceOptions[0].trim().split(',')
+      } else if (message.find((element) => element == 'IP-AUX1')) {
+        //This is an AUX list
+        this.instance.KairosObj.AUX.length = 0
+        message.forEach((element) => {
+          if (element !== '') this.instance.KairosObj.AUX.push({ aux: element, live: '', sources: [] })
+        })
+      } else if (message.find((element) => element.includes('MACROS.'))) {
+        //This is an MACRO list
+        this.instance.KairosObj.MACROS = message
+      } else if (message.find((element) => element.includes('Mixer.MV-Presets.'))) {
+        //This is an MV Preset list
+        this.instance.KairosObj.MV_PRESETS = message
+      } else if (message.find((element) => element.includes('SCENES.Main.Snapshots'))) {
+        //This is an SNAPSHOT list
+        this.instance.KairosObj.SNAPSHOTS = message
+      } else if (message.find((element) => element == 'SCENES.Main')) {
+        //This is an SCENES list
+        this.instance.KairosObj.SCENES = message
       } else {
         console.log(message)
       }
-
-      // this.instance.log('debug', `Command Response: ${message}`)
     })
   }
 
@@ -288,28 +274,12 @@ export class TCP {
     // @ts-expect-error Types doesn't include 'connected' property
     if (this.sockets.main && this.sockets.main.connected) {
       const message = `${command}\r\n`
-      if (message != '') console.log(message)
+      if (message != '\r\n') console.log(message)
 
       this.sockets.main.write(message, (err) => {
         if (err) this.instance.log('debug', err.message)
       })
-      this.instance.log('debug', `Sending command: ${message}`)
-    }
-  }
-  /**
-   * @param command function and any params
-   * @description Check TCP connection status and format command to send to Kairos
-   */
-  public readonly sendCommandSubscribeChannel = (command: string): void => {
-    // @ts-expect-error Types doesn't include 'connected' property
-    if (this.sockets.subscribe && this.sockets.subscribe.connected) {
-      const message = `${command}\r\n`
-      if (message != '') console.log(message)
-
-      this.sockets.subscribe.write(message, (err) => {
-        if (err) this.instance.log('debug', err.message)
-      })
-      this.instance.log('debug', `Sending command: ${message}`)
+      if (message != '\r\n') this.instance.log('debug', `Sending command: ${message}`)
     }
   }
 
