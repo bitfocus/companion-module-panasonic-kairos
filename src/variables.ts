ef454674de0d706d89b1bc87e288149073ff7c53
JeffreyDavidsz <jeffrey.davidsz@vicreo.eu>
2022-03-15 11:24:37 +0100

    extra variable

--- src/variables.ts
+++ src/variables.ts
@@ -51,12 +51,8 @@ export class Variables {
    * @description Sets variable definitions
    */
   public readonly updateDefinitions = (): void => {
-    const variables: Set<InstanceVariableDefinition> = new Set([
-      // Status
-      { label: 'Input on Main Background SourceA', name: 'main_background_sourceA' },
-      { label: 'Input on Main Background SourceB', name: 'main_background_sourceB' },
-    ])
-    const audio: Set<InstanceVariableDefinition> = new Set([
+
+    const audio_mute: Set<InstanceVariableDefinition> = new Set([
       // Status
       { label: 'Mute Master', name: 'mute_master_audio' },
       { label: 'Mute Channel 1', name: 'mute_channel_1' },
@@ -77,24 +73,34 @@ export class Variables {
       { label: 'Mute Channel 16', name: 'mute_channel_16' },
     ])
 
+		let inputSources = []
+		for (const input of this.instance.KairosObj.INPUTS) {
+			inputSources.push({label: `${input.input} name`, name: `${input.input}`})
+		}
+
 		let sceneNextTransition = []
 		for (const scene of this.instance.KairosObj.SCENES) {
-			sceneNextTransition.push({ label: `${scene.scene} next transition`, name: `${scene.scene}.next_transition` })
+			sceneNextTransition.push({ label: `${scene.scene.slice(7)} next transition`, name: `${scene.scene.slice(0,-1)}.next_transition` })
 		}
     let auxSources = []
     for (const AUX of this.instance.KairosObj.AUX) {
       auxSources.push({ label: `Source in ${AUX.aux}`, name: AUX.aux })
     }
+		let layerSources = []
+		for (const LAYER of this.instance.combinedLayerArray) {
+			layerSources.push({ label: `SourceA in ${LAYER.name.slice(7)}`, name: `${LAYER.name}.sourceA`})			
+			layerSources.push({ label: `SourceB in ${LAYER.name.slice(7)}`, name: `${LAYER.name}.sourceB`})			
+		}
     let auxAvailable = []
     for (const AUX of this.instance.KairosObj.AUX) {
       auxAvailable.push({ label: `${AUX.aux} available`, name: `${AUX.aux}.available` })
     }
     let playerRepeat = []
     for (const PLAYER of this.instance.KairosObj.PLAYERS) {
-      playerRepeat.push({ label: `${PLAYER.player} in repeat modus`, name: `${PLAYER.player}.${PLAYER.repeat}` })
+      playerRepeat.push({ label: `${PLAYER.player} in repeat modus`, name: `${PLAYER.player}.repeat` })
     }
 
-    let filteredVariables = [...variables, ...auxSources, ...audio, ...playerRepeat, ...auxAvailable, ...sceneNextTransition]
+    let filteredVariables = [...layerSources, ...sceneNextTransition, ...inputSources,...auxSources, ...audio_mute, ...playerRepeat, ...auxAvailable]
 
     this.instance.setVariableDefinitions(filteredVariables)
   }
@@ -105,40 +111,35 @@ export class Variables {
   public readonly updateVariables = (): void => {
     const newVariables: InstanceVariableValue = {}
 
-    // LIVE
-    newVariables['main_background_sourceA'] = this.instance.KairosObj.main_background_sourceA
-    newVariables['main_background_sourceB'] = this.instance.KairosObj.main_background_sourceB
-    //AUX
+    // LIVE LAYERS
+		for (const LAYER of this.instance.combinedLayerArray) {
+			newVariables[`${LAYER.name}.sourceA`] = this.instance.KairosObj.INPUTS.find(o => o.input === LAYER.sourceA)?.name
+			newVariables[`${LAYER.name}.sourceB`] = this.instance.KairosObj.INPUTS.find(o => o.input === LAYER.sourceB)?.name
+		}
+    // AUX
     for (const AUX of this.instance.KairosObj.AUX) {
-      newVariables[AUX.aux] = AUX.live
+      // newVariables[AUX.aux] = this.instance.KairosObj.INPUTS.find((x) => x.input == AUX.live)
+      newVariables[AUX.aux] = this.instance.KairosObj.INPUTS.find(o => o.input === AUX.liveSource)?.name
       newVariables[`${AUX.aux}.available`] = AUX.available == 0 ? 'disabled' : 'enabled'
     }
-		//TRANSITION
+		// TRANSITION
 		for (const SCENE of this.instance.KairosObj.SCENES) {
-      newVariables[`${SCENE.scene}.next_transition`] = SCENE.next_transition
+      newVariables[`${SCENE.scene}.next_transition`] = SCENE.next_transition.slice(SCENE.scene.length+1)
     }
-    // AUDIO
+		// PLAYERS
     for (const PLAYER of this.instance.KairosObj.PLAYERS) {
-      newVariables[`${PLAYER.player}.${PLAYER.repeat}`] = PLAYER.repeat == 0 ? 'repeat off' : 'repeat on'
+			newVariables[`${PLAYER.player}.repeat`] = PLAYER.repeat == 0 ? 'repeat off' : 'repeat on'
     }
+		// INPUTS
+		for (const INPUT of this.instance.KairosObj.INPUTS) {
+			newVariables[INPUT.input] = INPUT.name
+    }
+		// AUDIO
     newVariables['mute_master_audio'] = this.instance.KairosObj.audio_master_mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_1'] = this.instance.KairosObj.AUDIO_CHANNELS[0].mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_2'] = this.instance.KairosObj.AUDIO_CHANNELS[1].mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_3'] = this.instance.KairosObj.AUDIO_CHANNELS[2].mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_4'] = this.instance.KairosObj.AUDIO_CHANNELS[3].mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_5'] = this.instance.KairosObj.AUDIO_CHANNELS[4].mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_6'] = this.instance.KairosObj.AUDIO_CHANNELS[5].mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_7'] = this.instance.KairosObj.AUDIO_CHANNELS[6].mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_8'] = this.instance.KairosObj.AUDIO_CHANNELS[7].mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_9'] = this.instance.KairosObj.AUDIO_CHANNELS[8].mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_10'] = this.instance.KairosObj.AUDIO_CHANNELS[9].mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_11'] = this.instance.KairosObj.AUDIO_CHANNELS[10].mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_12'] = this.instance.KairosObj.AUDIO_CHANNELS[11].mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_13'] = this.instance.KairosObj.AUDIO_CHANNELS[12].mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_14'] = this.instance.KairosObj.AUDIO_CHANNELS[13].mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_15'] = this.instance.KairosObj.AUDIO_CHANNELS[14].mute == 0 ? 'unmuted' : 'muted'
-    newVariables['mute_channel_16'] = this.instance.KairosObj.AUDIO_CHANNELS[15].mute == 0 ? 'unmuted' : 'muted'
-
+		for (let index = 0; index < 16; index++) {
+			newVariables[`mute_channel_${index+1}`] = this.instance.KairosObj.AUDIO_CHANNELS[index].mute == 0 ? 'unmuted' : 'muted'
+		}
+    
     this.set(newVariables)
 
     this.updateDefinitions()
